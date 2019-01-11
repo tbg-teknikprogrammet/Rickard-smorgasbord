2bfb343bd1385b0cc52c8c90fd12cb727f2db465
Rickard Vestergren <Rickard.DTK.Vestergren@lystkom.se>
2019-01-11 14:53:27 +0100

    fixing me ToDo

--- projects/ToDo/assets/todo.js
+++ projects/ToDo/assets/todo.js
@@ -6,6 +6,7 @@
 // use while(), if() - elseif(), foreach(), for(), push(), pop(), slice(),
 
 // Create a "close" button and append it to each list item
+
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -26,14 +27,6 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
@@ -43,7 +36,7 @@ function newElement() {
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("Premade list").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
