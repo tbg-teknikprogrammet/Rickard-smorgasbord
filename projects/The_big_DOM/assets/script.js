b56d0e4ff5571e422b77c7250b3799524a02bc62
Rickard Vestergren <Rickard.DTK.Vestergren@lystkom.se>
2019-01-23 11:15:50 +0100

    The_BIg_DOM

---
+++ projects/The_big_DOM/assets/script.js
@@ -0,0 +1,9 @@
+// finna och förrendra ett element
+//SELECT an element and the MANIPULATE it
+
+// select
+var h1 = document.querySelector("h1");
+console.log(h1);
+
+//manipulate
+h1.style.color = "red";
